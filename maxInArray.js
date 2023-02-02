34f56572ea255a11e7a377e2c01079c4c47f85ae
MohonSaha <practicewithmohon@gmail.com>
2023-02-02 11:41:10 +0600

    find max number in array

---
+++ maxInArray.js
@@ -0,0 +1,20 @@
+
+// Problem: Take an array as input and return the largest Number. 
+
+
+function maxInArray(numbers){
+    let largest = numbers[0];
+    for(number in numbers){
+        const element = numbers[number];
+        if(element > largest){
+            largest = element;
+        }
+
+    }
+    return largest;
+}
+
+const heights = [10, 20, 30, 40, 90, 120, 340, 980];
+const result = maxInArray(heights);
+console.log(result);
+
